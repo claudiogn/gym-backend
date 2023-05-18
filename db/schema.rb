786ce445042d09358959abdf1db89c59049554aa
Claudio <claudioguimaraes14@gmail.com>
2023-05-17 21:39:53 -0300

    create exercise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_16_144930) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_18_003757) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_16_144930) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_exercises_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "type"
@@ -27,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_16_144930) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "exercises", "categories"
 end
