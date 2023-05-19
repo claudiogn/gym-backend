c803e10321a2e62220621aeb1341773c6cacd6e9
Claudio <claudioguimaraes14@gmail.com>
2023-05-18 21:50:24 -0300

    create users controllers

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::UsersController < Api::V1::BaseController
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :type)
+  end
+end
